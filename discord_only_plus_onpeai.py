78efc2bea7db9197c2da998f48ce7e8dcc8b4a76
Giuseppe V <102435860+Gvaldivia0309@users.noreply.github.com>
2026-06-02 08:32:21 -0500

    discord_only_plus_onpeai.py

--- discord_only_plus_onpeai.py
+++ discord_only_plus_onpeai.py
@@ -12,5 +12,48 @@ oa_client = OpenAI(api_key=OPENAI_KEY)
 def call_openai(question):
     # Call the OpenAI API
   completion = oa_client.chat.completions.create(
-    model=''
-  
+    model="gpt-4o",
+    messages=[
+        {
+            "role": "user",
+            "content": f"Respond like a AeroSpace" to the following question: {question}",
+        },
+        ]
+        )
+
+        #print the response
+        response = completion.choices[0].message.content
+        print(responde)
+        return response
+
+    #set up intents
+    intents = discord.Intents.default()
+    intents.message_content = True #Ensure that your bot can read message
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print('We have logged in as {0.user}'.format(client))
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        if message.content.startswitch('$hello'):
+            await message.channel.send('Hello!')
+
+        if massage.content.startswitch('$question'):
+            print(f"Message: {message.content}")
+            message content = message.content.split("$question")[1]
+            print(f"Question: {message_content}")
+            response = call_openai(message_content)
+            print(f"Assistant: {response}")
+            print("---")
+            await message.channel.send(response)
+
+client.run(os.getenv('TOKEN'))
+
+            
+        
+    
