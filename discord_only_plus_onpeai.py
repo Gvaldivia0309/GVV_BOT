afd7f1233ad8a81211e88c5332ba9e2d0e1d87e5
Giuseppe V <102435860+Gvaldivia0309@users.noreply.github.com>
2026-06-02 08:12:03 -0500

    discord_only_plus_onpeai.py

--- discord_only_plus_onpeai.py
+++ discord_only_plus_onpeai.py
@@ -1,1 +1,13 @@
+from dotenv import load_dotenv
+from openai import OpenAI # OpenAI library
+import discord
+import os
 
+#set openai api key
+load_dotenv()
+OPENAI_KEY = os.getenv('OPENAI_KEY')
+oa_client = OpenAI(api_key=OPENAI_KEY)
+
+#ask openai - respond like a AeroSpace
+def call_openai(question):
+  
